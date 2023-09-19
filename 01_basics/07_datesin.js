d93edbae3ad81a4f66be1130315dc29b4a645c81
Jitendra Kumar Jat <139442524+Jitendra1137@users.noreply.github.com>
2023-09-19 10:17:38 +0000

    02 basics

--- 01_basics/07_datesin.js
+++ 01_basics/07_datesin.js
@@ -1,10 +1,10 @@
 // Dates
 
 let myDate = new Date()
-// console.log(myDate.toString());
+console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+console.log(typeof myDate);
 
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
